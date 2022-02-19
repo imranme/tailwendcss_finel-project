fbee4ccfb0886b3ed93833e6d0cd7931976495ad
Tushar Imran <tusharimran914076@gmail.com>
2022-02-20 00:25:41 +0600

    creat a index ande some file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
+  },
+  plugins: [],
+}
